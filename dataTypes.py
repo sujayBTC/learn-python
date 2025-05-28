37774763adbb6a261596822a51b36321020b59d5
sujayBTC <Sujay.S@bigthinkcode.com>
2025-05-28 14:17:26 +0530

    finish datatypes and methods and change a file name learn.py into dataTypes.py

--- dataTypes.py
+++ dataTypes.py
@@ -23,7 +23,7 @@
 # print(list.count("orange"))->count get a parameter and check the array how many time the paramter in the array
 
 # list2=["chery","pineapple","watermeloan"]
-# list.extend(name)->adding a another array in last of the list or combine a two array
+# list.extend(list2)->adding a another array in last of the list or combine a two array
 
 # print(list.index("banana"))->it's return a index of first accurance
 
@@ -83,7 +83,7 @@
 # tuple-> tuble also contain a multiple element in a single variable 
 # tubles->tuble enclosed a (brackets)
 
-tuples=("apple","banana","mango","watermeloan","cherry")
+# tuples=("apple","banana","mango","watermeloan","cherry")
 
 # print(tuples)
 
@@ -102,3 +102,66 @@ tuples=("apple","banana","mango","watermeloan","cherry")
 # print(s1) #-> it's hold a first index element in tuples
 # print(s2) #-> it's hold a 2nd index element in tuples
 # print(s3) #-> it's hold a balance element in tuples
+
+# tuples=("kiwi","apple","watermelon","banana","apple")
+
+# print(tuples.count("apple"))->it's return how many time apple accurance in a tuples
+# print(tuples.index("kiwi")) -> return a first accurance index of kiwi
+
+# ********************SETS*************
+
+# sets={"apple","orange","banana","kiwi","apple","orange"} #creating a sets
+
+# sets.add("mango")->add a new element in last of sets
+
+# sets.clear()->clear the all elements
+
+# newSets=sets.copy()->copy the entire sets and stored a new variable
+# print(newSets)
+
+# sets2={"apple","orange","banana","kiwi","mango"}
+# print(sets2.difference(sets))->return a different element in sets2 compared in a sets
+# newsets=sets2-sets->do same things in difference
+# print(newsets)
+
+# sets.discard("watermeloan") #->remove a all kiwi element in a sets
+# sets.remove("watermelon")#->same as discard but raise a key error if you given element is not exist
+
+# print(sets.pop()) ->remove the elements randomly and return a remived item and also removed the duplicates
+
+# print(sets)
+
+
+
+# *******************Dictionaries****************
+
+# Dictionaries-->dictionaries likes objects in js 
+# Dictionaries stored a values in  key and pairs like-> "name":"sujay"-> dictonary key also enclosed whit "" double or single quates
+# Dictionaries are ordered, changable, do not allow a duplicates
+
+# Dictionary Creation--->
+# person={
+#     "name":"sujay",
+#     "age":23,
+#     "city":"mayiladuthurai"
+# }
+
+# Dictionary method
+
+# person.clear()-> clear the all element in a dictionary
+
+# newPer=person.copy()->copy and store a dictionary
+
+# ans=person.get("sujay")-> return a value based on you gives a key
+
+# ans=person.items() #return a tuples in key and values
+
+# ans=person.keys()->return a keys in a dictionary
+
+# person.pop("name")->remove the value and key based on key what you give
+
+# ans=person.popitem()->remove the last element and return that element
+# print(person)
+
+
+
